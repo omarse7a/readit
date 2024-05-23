927f07b5e6150e80afa152c7557309a87ba55437
Ibrahim Medhat <ibra.medhat@gmail.com>
2024-05-23 18:54:56 +0300

    Added search functionality
    -Without css

--- manageBooks/urls.py
+++ manageBooks/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.book_manager, name="book_list"),
-    path("book/<int:book_id>/", views.book_details, name="book_manage"),
+    path("book/<int:book_id>/", views.book_details, name="book_manager"),
 ]
