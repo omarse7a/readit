a86a22a8f9f72d1bf9732c667cfcf1978db2c17a
Ziad Ismael <ziad.ismael@icloud.com>
2024-05-24 21:24:52 +0300

    Functionality Fixed

--- borrowedBooks/views.py
+++ borrowedBooks/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from manageBooks.models import Book, Borrowed
+from django.http import JsonResponse
+from django.core import serializers
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def borrowedBooks(request):
-    return render(request, 'user-section/borrowedBooks.html')
+    current_user = request.user
+    userID = current_user.id
+    
+    return render(request, 'user-section/borrowedBooks.html', {'userID' : userID})
+
+def book_list(request):
+    books = list(Book.objects.values())
+    return JsonResponse(books, safe=False)
+    
+def borrowed_books_list(request):
+    borrowed_books = list(Borrowed.objects.values())
+    return JsonResponse(borrowed_books, safe=False)
+    
