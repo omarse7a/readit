0fc212039fb4ed8acb3b62eb4ed0db4e41436c8f
Khamis <mohamedkhamis20045@gmail.com>
2024-05-25 13:25:32 +0300

    fix some bugs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),  
     path('add-books/', views.addBooks, name='addbooks'),  
+    path('logout/',views. user_logout, name='logout'),
 ]
